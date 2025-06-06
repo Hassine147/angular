ab360e971fd3fa51eb709912dc7579518841d22c
hassinegreguiche13 <hassinegreguiche13@gmail.com>
2025-06-06 18:09:05 +0100

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'game', component: GameComponent },
+  { path: '**', redirectTo: 'login' }
+];
+
