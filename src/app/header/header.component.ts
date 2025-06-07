61a1f56a4ef9e75998f2fd4ff42e5b02d2c7477f
hassinegreguiche13 <hassinegreguiche13@gmail.com>
2025-06-07 14:32:15 +0100

    interface 2 logout et header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
+  standalone: true,  // ✅ This makes it a standalone component
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']  // ✅ Fix typo here
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit { 
+  constructor( private router: Router, private auth: AuthService) { }
+  ngOnInit(): void {
+    
+  }
+  goToHome() { 
+    this.router.navigate(['home']);
 
+  }
+
+  logout() {
+    console.log('Logout button clicked'); // ✅ Add log for debugging
+    this.auth.logout();
+
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  trendingGames: any;
+
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getTrendingGames();
+
+  }
+  getTrendingGames() {
+    this.http.get('assets/data/trending-games.json').subscribe((games) => {
+      this.trendingGames = games;
+      console.log(this.trendingGames)
+
+    });
+
+  }
 }
